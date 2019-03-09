99e01b2e8a00dd969813606363bf1142be1df4ae
Kyra J <kyraejaeger@gmail.com>
2019-03-09 18:56:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -42,8 +42,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array=array.slice(0, array.lenght-1);
-  return array
+  return array.slice(0, array.length-1);
 }
   
   
