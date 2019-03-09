d440e5423b8fce4d6f86205f3cf7c5edafe63202
Kyra J <kyraejaeger@gmail.com>
2019-03-09 18:23:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ var chocolateBars=[
   
   function destructivelyAddElementToEndOfArray(array, element){
     array.push(element);
-    return [array];
+    return array;
   }
   
   function accessElementInArray(){
